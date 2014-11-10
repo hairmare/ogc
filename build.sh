33d8f6cd21ea059f07a9c674857415a51ca8228f
Lucas Bickel <hairmare@purplehaze.ch>
2014-11-10 01:10:23 +0100

    fix user-data generation

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-
 service_tpl=$(cat <<EOD
     - name: %s\n
       command: start\n
@@ -12,6 +11,8 @@ service_awk=$(cat <<EOD
 BEGIN { printf("    - name: %s\n      command: start\n      enable: true\n      content: |\n", substr(name, 3)); }
 /^/ { printf("        %s\n", \$0); }
 EOD)
+disco_url=$(curl -q https://discovery.etcd.io/new)
 
 cat user-data.pre > user-data
+sed -i -e "s|DISCOVERY_URL|${disco_url}|" user-data
 find . -name '*.service' -exec awk -v name={} "${service_awk}" {} \; >> user-data
